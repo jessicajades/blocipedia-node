2eec095eea07f6af098649f8a595f12ec068da16
Jessica Shepherd <jessicashepherd@Jessicas-MacBook-Pro.local>
2019-07-12 12:23:23 -0700

    user sign in and out complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,5 +39,27 @@ module.exports = {
             }
         });
         sgMail.send(msg);
+    },
+
+    signInForm(req, res, next) {
+        res.render("users/signin");
+    },
+
+    signIn(req, res, next) {
+        passport.authenticate("local")(req, res, function() {
+            if (!req.user) {
+                req.flash("notice", "Sign in failed. Please try again.");
+                res.redirect("/users/signin");
+            } else {
+                req.flash("notice", "You've successfully signed in!");
+                res.redirect("/");
+            }
+        });
+    },
+
+    signOut(req, res, next) {
+        req.logout();
+        req.flash("notice", "You've successfully signed out!");
+        res.redirect("/");
     }
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,8 @@ const userController = require("../controllers/userController");
 
 router.get("/users/signup", userController.signup);
 router.post("/users/signup", validation.validateUsers, userController.create);
+router.get("/users/signin", userController.signInForm);
+router.post("/users/signin", validation.validateUsers, userController.signIn);
+router.get("/users/signout", userController.signOut);
 
 module.exports = router;
